08b8d46eaaf4c714bfa46031a5cf3082a3356cfa
Sanskrti Singh <sanskrti1910@gmail.com>
2026-06-05 23:58:26 +0530

    mongo issue

--- server/server.js
+++ server/server.js
@@ -1,20 +1,21 @@
-const express=require("express")
-const cors=require("cors")
-require("dotenv").config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
+const connectDB = require("./config/db");
 
+const app = express();
 
-const app=express()
+connectDB();
 
-app.use(cors())
-app.use(express.json())
-app.use("/api/auth",
-  require("./routes/authRoutes"))
+app.use(cors());
+app.use(express.json());
 
-app.use("/api/facts",require("./routes/factRoutes"))
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/facts", require("./routes/factRoutes"));
 
-const PORT=process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-console.log(`Server running on ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
