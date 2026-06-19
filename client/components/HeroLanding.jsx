005853f912e1171ebbeea72af8772d6c8759574c
Sanskrti Singh <sanskrti1910@gmail.com>
2026-06-19 22:34:54 +0530

    hero pg

---
+++ client/components/HeroLanding.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+import useTypewriter from "@/hooks/useTypewriter";
+
+export default function HeroLanding() {
+  // all your existing code
+}
